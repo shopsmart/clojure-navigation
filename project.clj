c2e1da28eef04c5aaa2fd660fcab8abc151c3a64
David Orme <dorme@bradsdeals.com>
2015-10-21 20:47:44 -0500

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
-(defproject bradsdeals.repl "0.1.0-SNAPSHOT"
-  :description "REPL utilities and experimental code"
-  :url "https://github.com/shopsmart/clojure-repl-tools"
+(defproject clojure-navigation "0.1.0-SNAPSHOT"
+  :description "Tools for navigating code and data structures from Clojure along with some generic helpers."
+  :url "https://github.com/shopsmart/clojure-navigation"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/tools.logging "0.2.6"]])
+                 [org.clojure/tools.logging "0.2.6"]]
+  :jar-name "clojure-navigation.jar"
+  :uberjar-name "clojure-navigation-uber.jar"
+  :jvm-opts ["-Xmx10g" "-Xms512m" "-XX:+UseParallelGC"]
+  :profiles {:uberjar {:aot :all}})
